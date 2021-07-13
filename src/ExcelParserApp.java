4b8d0c91121ed91d16358b533b76dcf17de32270
arsalaan <arsalaan0101@gmail.com>
2021-07-13 19:36:11 +0530

    changes made to parser

--- src/ExcelParserApp.java
+++ src/ExcelParserApp.java
@@ -32,11 +32,11 @@ public class ExcelParserApp {
             XSSFWorkbook wb = new XSSFWorkbook(fis);
             XSSFSheet ws = wb.getSheetAt(0);
 
-            int rowNum = ws.getLastRowNum() + 1;
+            int rowNum = ws.getLastRowNum();
             int colNum = ws.getRow(0).getLastCellNum();
 
             ArrayList<ArrayList<String>> rowData = new ArrayList<ArrayList<String>>();
-
+            int dupIE=0,dupCO=0,dupINC = 0;
             String colValStr = "";
             Iterator<Row> rowIterator = ws.rowIterator();
             //rowIterator.next();
@@ -63,23 +63,32 @@ public class ExcelParserApp {
                     list.add(colValStr);
 
                 }
-                if (list.contains("Infrastructure Event")
-                        || list.contains("Customer Owned")) {
+                if (list.contains("Infrastructure Event")) {
+                    dupIE++;
                     list.clear();
+
+                }
+                if (list.contains("Customer Owned")) {
+                    dupCO++;
+                    list.clear();
+
                 }
                 if (!list.isEmpty()) {
                     rowData.add(list);
                 }
             }
+
             int countRecord = 0;
             Map<String, ArrayList<String>> filterData = new LinkedHashMap<String, ArrayList<String>>();
-            ;
             for (ArrayList list : rowData) {
                 if (!filterData.containsKey(list.get(2))) {
                     filterData.put((String) list.get(2), list);
                     countRecord++;
+                }else{
+                    dupINC++;
                 }
             }
+
             int rowNumber = 0;
             // createnew workbook
             XSSFWorkbook workbook = new XSSFWorkbook();
@@ -92,8 +101,11 @@ public class ExcelParserApp {
             for (String key : keySet) {
                 Row row = sheet.createRow(rowNumber++);
                 List<String> nameList = filterData.get(key);
-                if(nameList.contains("Count:"))
+                if(nameList.contains("Count:")) {
+                    rowNum -= 1;
+                    countRecord -= 1;
                     continue;
+                }
                 int cellNumber = 0;
                 for (String s : nameList) {
                     Cell cell = row.createCell(cellNumber++);
@@ -115,6 +127,11 @@ public class ExcelParserApp {
             for (int i = 0; i < 15; i++) {
                 sheet.autoSizeColumn(i);
             }
+            System.out.println("Total Number Of Records in the Source File "+ (rowNum - 1));
+            System.out.println("Number of Infrastructure Event Removed "+ dupIE);
+            System.out.println("Number of Customer Owned Removed "+ dupCO);
+            System.out.println("Number of Duplicate Incident ID's Removed "+dupINC);
+            System.out.println("Number of Unique Records in Output File:" + (countRecord-1));
 
             FileOutputStream fos = new FileOutputStream(outputFileName);
             workbook.write(fos);
